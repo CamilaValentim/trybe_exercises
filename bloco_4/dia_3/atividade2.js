eed7ac7ad15eba5cbb9c1873841b9640f89ae7f7
Camila Valentim <camilaifpb@hotmail.com>
2020-09-01 11:59:35 -0300

    atividade triangulo invertido

--- bloco_4/dia_3/atividade2.js
+++ bloco_4/dia_3/atividade2.js
@@ -1,8 +1,12 @@
 let n = 5;
-let padrao = "";
+let padrao = "*";
 for (let i =0;i < n; i++){
-    padrao += "*";
     console.log(padrao);
+    for(j = 1;j < i; j++){
+        padrao += ("*")
+    }
 }
+
+
 
         
