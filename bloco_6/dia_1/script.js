2265a6d2a89e0aecb8708b6eb7fdd3b98f10a727
Camila Valentim <camilaifpb@hotmail.com>
2020-09-20 11:39:08 -0300

    initial commit

---
+++ bloco_6/dia_1/script.js
@@ -0,0 +1,17 @@
+let select = document.querySelector('#estados');
+console.log(select);
+
+let estados = ['Selecione o seu Estado','Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 
+'Distrito Federal', 'Espirito Santo' , 'Goiás', 'Maranhão', 'Mato Grosso do Sul', 'Mato Grosso',
+'Minas Gerais', 'Pará', 'Paraíba', 'Parana', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 
+'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Ronraima', 'Santa Catarina',
+'São Paulo', 'Sergipe', 'Tocantins'];
+
+for(let i = 0;i < estados.length; i +=1) {
+    let option = document.createElement('option');
+    option.setAttribute('name', 'estados');
+    option.innerText = estados[i];
+    select.append(option);
+}
+
+
