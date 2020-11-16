58cadc27167d75b8904aebba4e071f2159f675e6
Camila Valentim <camilaifpb@hotmail.com>
2020-11-16 14:56:35 -0300

    unindo componentes com estados e eventos

--- bloco_12/dia_1/exercicio-de-fixacao/src/Botao2.js
+++ bloco_12/dia_1/exercicio-de-fixacao/src/Botao2.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import Botao1 from './Botao1';
+
 
 class Botao2 extends React.Component {
     constructor(){
         super()
         this.eventButton2 = this.eventButton2.bind(this);
+        this.state = {
+            numeroDeClicks: 0
+
+        }
         
     }
     eventButton2() {
-        console.log(this);
-        console.log('click botao 2');
+        this.setState((estadoAnterior, _props) => ({
+            numeroDeClicks: estadoAnterior.numeroDeClicks + 1
+        }))
+        
     }
     render() {
+        console.log(this);
         return (
             <button className='Botao2' onClick={this.eventButton2}>Botao2</button>
         ) 
