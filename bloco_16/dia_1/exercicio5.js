4c56aca88d08162f7547a00b99688448fd750291
Camila Valentim <camilaifpb@hotmail.com>
2021-01-01 13:21:34 -0300

    Use const for Action Types

---
+++ bloco_16/dia_1/exercicio5.js
@@ -0,0 +1,47 @@
+// Change code below this line
+
+// Change code above this line
+const loginUser = () => {
+    return {
+      type: "LOGIN"
+    };
+  };
+
+const defaultState = {
+    authenticated: false
+  };
+  
+  const authReducer = (state = defaultState, action) => {
+  
+    switch (action.type) {
+  
+      case 'LOGIN':
+        return {
+          authenticated: true
+        }
+  
+      case 'LOGOUT':
+        return {
+          authenticated: false
+        }
+  
+      default:
+        return state;
+  
+    }
+  
+  };
+  
+  const store = Redux.createStore(authReducer);
+  
+  const loginUser = () => {
+    return {
+      type: 'LOGIN'
+    }
+  };
+  
+  const logoutUser = () => {
+    return {
+      type: 'LOGOUT'
+    }
+  };
